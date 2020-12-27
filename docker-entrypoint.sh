de8d013830756243db197895755e9db45793b221
wuttinanhi <wuttinanhi@gmail.com>
2020-12-27 17:16:19 +0700

    change and clean

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,9 @@
 
 FILE=/sshkey/id_rsa.pub
 if [ -f "$FILE" ]; then
-    echo "key will not generate because file alreay exists."
-else 
+    echo "key will not generate because files already exists."
+else
     echo "Generating key..."
-    (cat /dev/zero | ssh-keygen -m PEM -t rsa -q -N "" -f "/root/.ssh/id_rsa")> /dev/null 2>&1
-    
-    mv ~/.ssh/id_rsa.pub /sshkey
-    mv ~/.ssh/id_rsa /sshkey
-
+    (cat /dev/zero | ssh-keygen -q -m "$KEY_FORMAT" -t "$TYPE" -f "$FILENAME" -N "$PASSPHRASE" -C "$COMMENT")> /dev/null 2>&1
     echo "done."
 fi
